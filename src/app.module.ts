4eb406f27a62f8dfad047c1041864524bed82f54
hossain-riyad <hossain.riyad@sslwireless.com>
2023-06-06 10:29:46 +0600

    Prepare schema of bookmark, and user, connect database, and migrate database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './g/prisma/prisma.service';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
